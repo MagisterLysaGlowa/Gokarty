90a0dd5bc1852d349fb4901bb400978b29c70f66
Bart1251 <bartlomiej.wiecek@vp.pl>
2025-03-26 19:27:47 +0100

    Add buttons for starting ending and creating a queue for tournament

--- frontend/src/pages/TournamentEdit/TournamentEditPages/tournamentInfo/TournamentInfo.tsx
+++ frontend/src/pages/TournamentEdit/TournamentEditPages/tournamentInfo/TournamentInfo.tsx
@@ -1,5 +1,5 @@
 import { Button, useDisclosure } from "@heroui/react";
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaDice, FaEdit, FaPlay, FaStop, FaTrash } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { TournamentData, TournamentType } from "../../../../../types";
@@ -8,12 +8,17 @@ import { RemoveTournamentModal } from "./tournamentInfoComponents/RemoveTourname
 import { EditTournamentModal } from "./tournamentInfoComponents/EditTournamentModal";
 import "../../tournamentEdit.css";
 import { TournamentInfoComponent } from "./tournamentInfoComponents/TournamentInfoComponent";
+import { Loading } from "../../../../components/Loading/Loading";
+import { CreateQueueModal } from "./tournamentInfoComponents/CreateQueueModal";
+import { StartEndTournamentModal } from "./tournamentInfoComponents/StartEndTournamentModal";
 
 export const TournamentInfo = () => {
   const { id } = useParams();
 
   const editModal = useDisclosure();
   const removeModal = useDisclosure();
+  const queueModal = useDisclosure();
+  const startEndModal = useDisclosure();
 
   const [tournament, SetTournament] = useState<TournamentData>({
     endDate: new Date(),
@@ -31,40 +36,61 @@ export const TournamentInfo = () => {
 
   return (
     <div className="grid place-items-center h-full">
-      <TournamentInfoComponent
-        tournament={tournament}
-        isLoading={isLoading}
-        key={id}
-      />
-      <div className="adminActions">
-        <Button
-          className="tournamentButton bg-main-default"
-          onPress={() => editModal.onOpen()}
-          endContent={<FaEdit />}
-          isIconOnly
+      {isLoading ? 
+      <Loading isLoading={isLoading}/> : 
+      <>
+        <TournamentInfoComponent
+          tournament={tournament}
+          isLoading={isLoading}
+          key={id}
         />
-        <Button
-          isIconOnly
-          endContent={<FaTrash />}
-          className="tournamentButton bg-red-600"
-          onPress={() => removeModal.onOpen()}
+        <div className="adminActions">
+          <Button
+            className="tournamentButton bg-main-default"
+            onPress={() => editModal.onOpen()}
+            endContent={<FaEdit />}
+            isIconOnly
+          />
+          <Button
+            isIconOnly
+            endContent={<FaTrash />}
+            className="tournamentButton bg-red-600"
+            onPress={() => removeModal.onOpen()}
+          />
+          {tournament.tournamentStateId != 3 &&
+          <Button
+            isIconOnly
+            endContent={tournament.tournamentStateId == 1 ? <FaPlay /> : <FaStop />}
+            className="tournamentButton bg-blue-600"
+            onPress={() => startEndModal.onOpen()}
+          />}
+          {tournament.tournamentStateId == 2 &&
+          <Button
+            isIconOnly
+            endContent={<FaDice />}
+            className="tournamentButton bg-orange-600"
+            onPress={() => queueModal.onOpen()}
+          />}
+        </div>
+        <RemoveTournamentModal
+          modal={removeModal}
+          tournament={tournament}
         />
-        <Button
-          isIconOnly
-          endContent={<FaTrash />}
-          className="tournamentButton bg-red-600"
-          onPress={() => removeModal.onOpen()}
+        <EditTournamentModal
+          modal={editModal}
+          tournament={tournament}
+          setTournament={SetTournament}
         />
-      </div>
-      <RemoveTournamentModal
-        modal={removeModal}
-        tournament={tournament}
-      />
-      <EditTournamentModal
-        modal={editModal}
-        tournament={tournament}
-        setTournament={SetTournament}
-      />
+        <CreateQueueModal
+          tournament={tournament}
+          modal={queueModal}
+        />
+        <StartEndTournamentModal
+          modal={startEndModal}
+          tournament={tournament}
+        />
+      </>
+      }
     </div>
   );
 };
---
+++ frontend/src/pages/TournamentEdit/TournamentEditPages/tournamentInfo/tournamentInfoComponents/CreateQueueModal.tsx
@@ -0,0 +1,95 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  NumberInput,
+  Select,
+  SelectItem,
+} from "@heroui/react";
+import { ModalProps, TournamentData } from "../../../../../../types";
+import { useState } from "react";
+import { GokartQueries } from "../../../../../queries/gokartQuery";
+import { QueueQueries } from "../../../../../queries/queueQuery";
+import { queueValidation } from "../../../../../validations/QueueRandomizationValidation";
+
+type CreateQueueProps = {
+  modal: ModalProps;
+  tournament: TournamentData;
+};
+
+export const CreateQueueModal: React.FC<CreateQueueProps> = ({
+  modal,
+  tournament
+}) => {
+  const [numberOfRidesInOneGokart, setNumberOfRidesInOneGokart] = useState<number>(1);
+  const [gokartIds, setGokartIds] = useState<number[]>([]);
+
+  const { data: gokarts } = GokartQueries.getAllGokarts();
+  const { mutateAsync: createQueuesAsync } = QueueQueries.createQueue();
+
+  const variant = "underlined";
+
+  if(!gokarts) return;
+
+  return (
+    <Modal placement="top-center" {...modal} key={tournament.tournamentId}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              Wylosuj kolejkę
+            </ModalHeader>
+            <ModalBody>
+              <NumberInput
+                min={1}
+                type="number"
+                label="Ilość przejazdów na gokart"
+                value={numberOfRidesInOneGokart}
+                variant={variant}
+                onValueChange={setNumberOfRidesInOneGokart}
+              />
+              <Select
+                label="Wybierz gokarty"
+                selectedKeys={gokartIds}
+                selectionMode="multiple"
+                variant={variant}
+                onSelectionChange={(e) => setGokartIds(Array.from(e as Set<number>))}
+              >
+                {gokarts.map(g => 
+                    <SelectItem key={g.gokartId}>{g.name}</SelectItem>
+                )}
+              </Select>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="flat" onPress={onClose}>
+                Anuluj
+              </Button>
+              <Button
+                className="bg-main-default"
+                onPress={async () => {
+                  if(await queueValidation({
+                    gokartIds: gokartIds,
+                    numberOfRidesInOneGokart: numberOfRidesInOneGokart,
+                    tournamentId: tournament.tournamentId
+                  })) {
+                    await createQueuesAsync({
+                      gokartIds: gokartIds,
+                      numberOfRidesInOneGokart: numberOfRidesInOneGokart,
+                      tournamentId: tournament.tournamentId
+                    });
+                  }
+                  onClose();
+                }}
+              >
+                Zatwierdź
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
